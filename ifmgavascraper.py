3c14e1c060dce87c31b917d00425481210c1e3d9
Warley <warleysoaresr@gmail.com>
2022-08-18 16:49:11 -0300

    remocao de restricao de senha e atualizacao cursos semestre atual

--- ifmgavascraper.py
+++ ifmgavascraper.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+from datetime import date, datetime
+import re
 
 
 def get_grades(ra, senha):
@@ -14,26 +13,35 @@ def get_grades(ra, senha):
     """
     session = requests.Session()
 
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                             + "Chrome/47.0.2526.111 Safari/537.36"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        + "Chrome/47.0.2526.111 Safari/537.36"
+    }
 
     # Requisição inicial para obter o "logintoken"
     login_url = "https://ava.betim.ifmg.edu.br/moodle/login/index.php"
     req = session.get(login_url, headers=headers, verify=False)
-    token = BeautifulSoup(req.content, "html.parser").find("input", {"name": "logintoken"})["value"]
+    token = BeautifulSoup(req.content, "html.parser").find(
+        "input", {"name": "logintoken"}
+    )["value"]
 
     # Requisição POST para realizar login no AVA, passando os dados de login
     payload = {"username": ra, "password": senha, "logintoken": token}
     req = session.post(login_url, data=payload, headers=headers, verify=False)
 
-    aluno = BeautifulSoup(req.content, "html.parser").find("span", {"class": "usertext mr-1"})
+    aluno = BeautifulSoup(req.content, "html.parser").find(
+        "span", {"class": "usertext mr-1"}
+    )
     if aluno is None:
         return None
     aluno = aluno.get_text()
 
     # Requisição para obter lista de cursos e os links para analisar notas
-    req = session.get("https://ava.betim.ifmg.edu.br/moodle/grade/report/overview/index.php?id=318",
-                      cookies=req.cookies.get_dict(), verify=False)
+    req = session.get(
+        "https://ava.betim.ifmg.edu.br/moodle/grade/report/overview/index.php?id=318",
+        cookies=req.cookies.get_dict(),
+        verify=False,
+    )
 
     # Obter tabela e extrair informações com o "parser" de HTML
     soup = BeautifulSoup(req.content, "html.parser")
@@ -43,10 +51,15 @@ def get_grades(ra, senha):
     if tabela is None:
         return tabela
 
+    today = datetime.today()
+    current_semester = f"{today.year}.{1 if today.month <= 7 else 2}"
+
     cursos = tabela.find_all("td", {"class": "cell c0"})
     cursos = [c.find("a") for c in cursos]
-    nomes = [c.contents for c in cursos if c is not None]
-    links = [c["href"] for c in cursos if c is not None]
+    nomes = [
+        c.text for c in cursos if c is not None and re.match(current_semester, c.text)
+    ]
+    links = [c["href"] for c in cursos if c.text in nomes]
 
     return dict(aluno=aluno, cursos=nomes, links=links), session, req.cookies.get_dict()
 
@@ -59,11 +72,11 @@ def get_specific_grades(data, session, cookies):
     :param cookies: Cookies usados no login inicial
     :return: Dicionário com aluno e cursos dict {chave nome e valor (atividade, nota)}
     """
-    grades = dict(aluno=data['aluno'], cursos={})
+    grades = dict(aluno=data["aluno"], cursos={})
 
-    for i in range(len(data['cursos'])):
-        curso = data['cursos'][i][0]
-        link = data['links'][i]
+    for i in range(len(data["cursos"])):
+        curso = data["cursos"][i]
+        link = data["links"][i]
 
         # Requisição para o link específico do curso
         req = session.get(link, cookies=cookies, verify=False)
@@ -75,18 +88,17 @@ def get_specific_grades(data, session, cookies):
 
         # Extrair atividades
         atividades = []
-        for atividade in body.find_all('th'):
-            atv = atividade.find('a')
+        for atividade in body.find_all("th"):
+            atv = atividade.find("a")
             if atv is not None:
                 atividades.append(atv.get_text())
 
         # Extrair notas
         notas = []
-        for nota in body.find_all('td', class_='column-grade'):
+        for nota in body.find_all("td", class_="column-grade"):
             notas.append(nota.get_text())
 
         # Associar atividades e notas e colocar no dicionario
-        grades['cursos'][curso] = list(zip(atividades, notas))
+        grades["cursos"][curso] = list(zip(atividades, notas))
 
     return grades
-
--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-""""
-IFMG Notas - Aplicativo (não muito útil) para verificar as notas no AVA (Moodle)
-de cursos do Campus Betim, sem precisar ficar fazendo login
-"""
 from PySimpleGUI import PySimpleGUI as Sg
 from pathlib import Path
 import ifmgavascraper as ava
@@ -10,7 +6,7 @@ import os
 
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+    """Get absolute path to resource, works for dev and for PyInstaller"""
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
@@ -33,13 +29,27 @@ def login_window():
     Sg.theme("Reddit")
     font_str = "Helvitica 15 bold"
     layout = [
-        [Sg.Image(resource_path(".img\\ifmglogo.png"))],
+        [Sg.Image(resource_path("ifmglogo.png"))],
         [Sg.Text("RA:", font=font_str, text_color="green")],
-        [Sg.Input(key="-RA-", font=font_str, size=(25, 1), default_text=user if user else "")],
+        [
+            Sg.Input(
+                key="-RA-",
+                font=font_str,
+                size=(25, 1),
+                default_text=user if user else "",
+            )
+        ],
         [Sg.Text("Senha:", font=font_str, text_color="green")],
         [Sg.Input(key="-PASS-", password_char="•", font=font_str, size=(25, 1))],
-        [Sg.Checkbox("Salvar login", key="-SAVE-", font="Helvitica 12", default=True if user else False)],
-        [Sg.Submit("Buscar notas", font=font_str, button_color=("white", "green"))]
+        [
+            Sg.Checkbox(
+                "Salvar login",
+                key="-SAVE-",
+                font="Helvitica 12",
+                default=True if user else False,
+            )
+        ],
+        [Sg.Submit("Buscar notas", font=font_str, button_color=("white", "green"))],
     ]
     return Sg.Window("Notas IFMG - AVA Betim", layout)
 
@@ -50,27 +60,35 @@ def grades_window(grades_dict):
     tab_group = []
 
     i = 1
-    for curso in grades_dict['cursos']:
-        notas = grades_dict['cursos'][curso]
+    for curso in grades_dict["cursos"]:
+        notas = grades_dict["cursos"][curso]
 
         if len(notas) == 0:
-            notas.append(('', ''))
+            notas.append(("", ""))
 
-        tab_lay = [[Sg.Text(curso, font="Helvitica 16 bold", text_color="#5534eb")],
-                   [Sg.Table(values=notas,
-                             headings=["Atividade", "Nota"],
-                             background_color="lightgray",
-                             alternating_row_color="lightgreen",
-                             font="Helvitica 12 bold",
-                             auto_size_columns=True,
-                             max_col_width=50)]]
+        tab_lay = [
+            [Sg.Text(curso, font="Helvitica 16 bold", text_color="#5534eb")],
+            [
+                Sg.Table(
+                    values=notas,
+                    headings=["Atividade", "Nota"],
+                    background_color="lightgray",
+                    alternating_row_color="lightgreen",
+                    font="Helvitica 12 bold",
+                    auto_size_columns=True,
+                    max_col_width=50,
+                )
+            ],
+        ]
         tab = [Sg.Tab(f"Curso {i}", tab_lay)]
         tab_group.append(tab)
         i += 1
 
     layout = [[Sg.TabGroup(tab_group)]]
 
-    return Sg.Window(f"{grades_dict['aluno']} - Notas IFMG - AVA Betim", layout, resizable=True)
+    return Sg.Window(
+        f"{grades_dict['aluno']} - Notas IFMG - AVA Betim", layout, resizable=True
+    )
 
 
 # Execução do programa
@@ -83,14 +101,24 @@ while True:
         ra = values["-RA-"]
         senha = values["-PASS-"]
 
-        if ra == '' or senha == '':
-            Sg.popup_error("Preencha os campos RA e senha!",
-                           title="Erro de login", background_color="gray", text_color="white", font="Helvitica 12 bold")
+        if ra == "" or senha == "":
+            Sg.popup_error(
+                "Preencha os campos RA e senha!",
+                title="Erro de login",
+                background_color="gray",
+                text_color="white",
+                font="Helvitica 12 bold",
+            )
             continue
 
-        if not ra.isnumeric() or not senha.isnumeric():
-            Sg.popup_error("Os campos são numéricos!",
-                           title="Erro de login", background_color="gray", text_color="white", font="Helvitica 12 bold")
+        if not ra.isnumeric():
+            Sg.popup_error(
+                "O campo RA é numérico!",
+                title="Erro de login",
+                background_color="gray",
+                text_color="white",
+                font="Helvitica 12 bold",
+            )
             continue
 
         save = values["-SAVE-"]
@@ -108,8 +136,13 @@ while True:
         lw.hide()
         login = ava.get_grades(ra, senha)
         if login is None:
-            Sg.popup_error("Não foi possível obter suas informações. Confira o RA e senha e tente novamente.",
-                           title="Erro de login", background_color="gray", text_color="white", font="Helvitica 12 bold")
+            Sg.popup_error(
+                "Não foi possível obter suas informações. Confira o RA e senha e tente novamente.",
+                title="Erro de login",
+                background_color="gray",
+                text_color="white",
+                font="Helvitica 12 bold",
+            )
             lw.un_hide()
         else:
             grades = ava.get_specific_grades(*login)
